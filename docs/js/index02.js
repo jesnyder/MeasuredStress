7c205249575dc0e20002298c5de650edbbe2dd38
jesnyder <jesyndre@gmail.com>
2021-08-17 16:44:34 -0400

    plot ACC data from csv on github

--- docs/js/index02.js
+++ docs/js/index02.js
@@ -1,40 +1,48 @@
 fetch('https://raw.githubusercontent.com/jesnyder/MeasuredStress/main/studies/HI/source/1542117214_A01172/ACC.csv')
-	.then(function (response) {
-		return response.text();
-	})
-	.then(function (text) {
-		let series = csvToSeries(text);
-		renderChart(series);
-	})
-	.catch(function (error) {
-		//Something went wrong
-		console.log(error);
-	});
+.then(function (response) {
+	return response.text();
+})
+.then(function (text) {
+	let series = csvToSeries(text);
+	renderChart(series);
+})
+.catch(function (error) {
+	//Something went wrong
+	console.log(error);
+});
 
 function csvToSeries(text) {
-	
-	const col01 = '1542117214.000000';
 
-	let dataAsJson = JSC.csv2Json(text);
-	let male = [], female = [];
-	dataAsJson.forEach(function (row) {
-		//add either to male, female, or discard.
-		if (row.race === 'All Races') {
-			if (row.sex === 'Male') {
-				male.push({x: row.year, y: row[lifeExp]});
-			} else if (row.sex === 'Female') {
-				female.push({x: row.year, y: row[lifeExp]});
-			}
-		}
-	});
-	return [
-		{name: 'Male', points: male},
-		{name: 'Female', points: female}
-	];
+const lifeExp = 'average_life_expectancy';
+var inc = 1;
+
+let dataAsJson = JSC.csv2Json(text);
+let time = [], meas = [];
+dataAsJson.forEach(function (row) {
+
+	console.log(row)
+	console.log(row[1])
+
+
+
+	if (row[1] < 10000){
+	meas.push({x: inc, y: row[0]});
+	inc = inc+1;
+	console.log(inc)
+	}
+
+});
+
+console.log(meas)
+
+return [
+	{name: 'Meas', points: meas},
+	{name: 'Female', points: female}
+];
 }
 
 function renderChart(series) {
-	JSC.Chart('chartDiv', {
-		series: series
-	});
+JSC.Chart('chartDiv', {
+	series: series
+});
 }
