7018cab8ef5cdd40279e5e967f1dcde34c162929
jesyndre@gmail.com <jesyndre@gmail.com>
2022-11-09 23:20:35 -0800

    editted nav bar

--- docs/js/draw2.js
+++ docs/js/draw2.js
@@ -1,11 +1,20 @@
-var canvases = document.getElementsByTagName('canvas');
 
-  for( var i=0; i<canvases.length; i++){
-       ctx = canvases[i].getContext('2d');
+  // example https://www.w3resource.com/javascript-exercises/javascript-drawing-exercises.php
 
-       ctx.arc(50, 50, 50, 0, 1.5*Math.PI);
-       ctx.lineWidth = 15;
+  var canvases = document.getElementsByTagName('canvas');
 
-       ctx.strokeStyle = 'black';
-       ctx.stroke();
-};
+    for( var i=0; i<canvases.length; i++){
+         ctx = canvases[i].getContext('2d');
+
+         multp = i*1
+
+         // Outer circle
+         ctx.arc(75+multp*10,75+multp*10,50+multp*10,0,Math.PI*2,true);
+         ctx.moveTo(110,75);
+
+         ctx.stroke();
+         ctx.lineWidth = 10;
+
+         ctx.strokeStyle = 'black';
+         ctx.stroke();
+  }
